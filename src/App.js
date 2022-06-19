9a968d4d61e6b0e54c36a88fead59fac26dab579
Tabu Hana <taboodog@hotmail.com>
2022-06-18 18:59:07 -0700

    routes; navbar init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import React from 'react'
+import { ThemeProvider } from "./context/ThemeContext";
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div>
-      tset
-    </div>
+    <ThemeProvider>
+      <Navbar />
+    </ThemeProvider>
   );
 }
 
