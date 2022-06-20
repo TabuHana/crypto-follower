fd9801ed23f00d6356cd1ff174b2855cc8c81ade
Tabu Hana <taboodog@hotmail.com>
2022-06-20 08:41:21 -0700

    api add;  setting search

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ThemeProvider } from "./context/ThemeContext";
 import Navbar from './components/Navbar'
 import { Route, Routes } from 'react-router-dom';
@@ -6,8 +6,21 @@ import Home from './routes/Home';
 import UserLogin from './routes/UserLogin';
 import UserSignup from './routes/UserSignup';
 import Account from './routes/Account';
+import axios from "axios";
 
 function App() {
+  const [coin, setCoin] = useState([])
+
+  // has to be a string. no hidden key so leaving here x.x
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=12&page=1&sparkline=true'
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoin(response.data)
+      console.log(response.data)
+    })
+  },[url])
+
   return (
     <ThemeProvider>
       <Navbar />
