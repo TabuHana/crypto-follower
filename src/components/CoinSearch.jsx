49f95948945b052cade640f5cfeaf67648561d42
Tabu Hana <taboodog@hotmail.com>
2022-06-20 10:03:57 -0700

    styling

--- src/components/CoinSearch.jsx
+++ src/components/CoinSearch.jsx
@@ -1,37 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CoinTool from './CoinTool';
 
 const CoinSearch = ({ coins }) => {
 
+  const [search, setSearch] = useState('')
 
   return (
-    <div>
-      <div>
-        <h1>Search Crypto</h1>
+    <div className='rounded-div my-4'>
+      <div className='flex flex-col md:flex-row justify-between pt-44 pb-6 text-center md:text-right'>
+        <h1 className='text-2xl font-bold my-2'>Search Crypto</h1>
         <form>
-          <input type='text' placeholder='Search coin' />
+          <input
+            onChange={(e) => setSearch(e.target.value)}
+            type='text'
+            placeholder='Search coin'
+            className='w-full bg-primary border border-input px-4 py-2 rounded-2xl shadow-xl' />
         </form>
       </div>
 
-      <table>
+      <table className='w-full border-collapse text-center'>
         <thead>
-          <tr>
+          <tr className='border-b'>
             <th></th>
-            <th>#</th>
-            <th>Coin</th>
+            <th className='px-4'>#</th>
+            <th className='text-left'>Coin</th>
             <th></th>
             <th>Price</th>
             <th>24h</th>
-            <th>24h Volume</th>
-            <th>Market</th>
+            <th className='hidden md:table-cell'>24h Volume</th>
+            <th className='hidden sm:table-cell'>Market</th>
             <th>Last 7 days</th>
           </tr>
         </thead>
 
         <tbody>
-          {coins.map((coin) => (
-            <CoinTool key={coin.id} coin={coin} />
-          ))}
+          {coins.filter((value) => {
+            if (search === '') {
+              return value
+            } else if (value.name.toLowerCase().includes(search.toLowerCase())) {
+              return value
+            } else {
+              return value
+            }
+          })
+            .map((coin) => (
+              <CoinTool key={coin.id} coin={coin} />
+            ))}
         </tbody>
 
       </table>
