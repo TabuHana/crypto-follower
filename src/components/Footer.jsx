abef8e51a869cc56240641c0d3b1f7f1d1e9ab47
Tabu Hana <taboodog@hotmail.com>
2022-06-20 20:08:52 -0700

    footer create

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ThemeToggle from "./ThemeToggle";
+import { AiOutlineInstagram } from "module";
+import {  } from "module";
+
+const Footer = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
