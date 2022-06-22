6324517e93a5218c8364aee5d008fb0d3569f996
Tabu Hana <taboodog@hotmail.com>
2022-06-22 03:42:03 -0700

    fixed .env

--- src/firebase.js
+++ src/firebase.js
@@ -6,22 +6,13 @@ import { getFirestore } from 'firebase/firestore';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER,
-//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCJP5a1OR0V3CCYoUQyG1FEfKpSeO7DFpw",
-  authDomain: "cryptofollower-ca7ce.firebaseapp.com",
-  projectId: "cryptofollower-ca7ce",
-  storageBucket: "cryptofollower-ca7ce.appspot.com",
-  messagingSenderId: "81224749933",
-  appId: "1:81224749933:web:1c7c8fe9e500c11d494064"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
